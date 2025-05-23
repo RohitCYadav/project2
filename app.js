64bce662d5c0d4999f3c8fb68c17abfe27492115
RohitCYadav <rohitcyadav29@gmail.com>
2025-05-23 21:56:37 +0530

    js file code added

---
+++ app.js
@@ -0,0 +1,9 @@
+// script.js - Simple JavaScript Example
+
+// Display "Hello, World!" in the console
+console.log("Hello, World!");
+
+// Function to change the text of an element
+function changeMessage() {
+    document.getElementById("message").innerText = "JavaScript is awesome!";
+}
